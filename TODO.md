9526c56c74a5a9030510c2aa0d49649577d1ba28
Milos Milosavljevic <milosh@emisia.net>
2025-09-11 23:04:54 +0200

    update

---
+++ TODO.md
@@ -0,0 +1,29 @@
+- [X] starship
+- [X] nuscripts (farcic)
+- [x] devbox
+- [x] dotfiles, sync (farcic)
+- [x] zellij
+- [ ] fabric (env)
+- [x] iterm2
+- [x] cleanup devbox.json and default devbox.json
+- [ ] renovate for versions
+- [x] uninstall.sh script
+- [x] VSCode extensions
+- [x] VSCode themes (auto)
+- [ ] Floccus via Git (bookmark sync)
+- [ ] delete passwords from git history
+
+## Devbox
+Global:
+- [x] tor-browser
+- [x] brave ( +extensions)
+- [ ] Podman/Docker
+- [ ] Wireguard VPN
+- [x] Visual Studio Code
+
+Shell:
+- [ ] bitwarden cli
+- [ ] summon
+- [x] gh cli
+- [ ] torsocks
+- [x] GitHub SSH key (insecure)
---
+++ install-optional.sh
@@ -0,0 +1,25 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask visual-studio-code
+
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension mhutchie.git-graph
+
+brew install --cask tor-browser
+brew install brave-browser
+brew install vlc
+brew install transmission
+brew install cloudflare-warp
+
+#Very optional:
+#brew install --cask sublime-text
+#brew install midnight-commander
+#brew install --cask commander-one
+#brew install --cask plex-media-server
+#brew install --cask caffeine
+#brew install --cask balenaetcher
+#brew install --cask iterm2
+#brew install --cask resilio-sync
+#brew install --cask signal
+#brew install --cask virtualbox
+#brew install yt-dlp
